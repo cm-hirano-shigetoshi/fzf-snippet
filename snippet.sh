ea6b739d69787238d781437fdd8d5213af6f7c82
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2019-04-27 19:08:30 +0900

    readlinkを自前のものに

--- snippet.sh
+++ snippet.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 set -eu
 
-readonly TOOLDIR=$(dirname $(readlink -e $0))
+function readlink_e() {
+  local p
+  if ! p=$(readlink -e "$1" 2>/dev/null); then
+    p="$1"
+    while [[ -L "$p" ]]; do
+      p=$(readlink "$p")
+    done
+  fi
+  echo $p
+}
+
+
+readonly TOOLDIR=$(dirname $(readlink_e $0))
 
 fzfer run ${TOOLDIR}/snippet.yml \
   | perl ${TOOLDIR}/lib/extract_lines.pl \
